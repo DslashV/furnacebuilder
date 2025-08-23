c6b434db7a170dc504c0c3d6067eb963ea2bf0c4
D/V <128831268+DslashV@users.noreply.github.com>
2025-08-23 17:43:44 +0300

    included "IF EXIST "BUILD/" MKDIR BUILD"

--- furnacebuilder.bat
+++ furnacebuilder.bat
@@ -3,8 +3,9 @@ rem downloads furnace using git
 git clone --recursive https://github.com/tildearrow/furnace.git
 rem cd to the furnace dir and makes build folder and builds and complies using cmake and make
 cd furnace
-mkdir build
+IF EXIST "BUILD/" MKDIR BUILD
 cd build
 cmake -G "Unix Makefiles" ..
 echo now run 'make' in your command prompt
 exit /b
+
